36d5b446e89be224218b4f1eb98cb9f7b343ae08
kundyli <kundyli@163.com>
2019-07-06 21:49:10 +0800

    feat:山脉数组的峰顶索引

---
+++ src/PeakIndexInMountainArray.java
@@ -0,0 +1,24 @@
+/**
+ * 山脉数组的峰顶索引
+ * <p>
+ * 需求：
+ * 找出数组中最大的元素，该元素位于数组的中间。
+ * <p>
+ * 解题思路：
+ * 出现第一个A[i] > A[i+1] 是，i便是最大值位置。
+ *
+ * @author kundy
+ * @date 2019/7/6 3:19 PM
+ */
+public class PeakIndexInMountainArray {
+
+    public int peakIndexInMountainArray(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > A[i + 1]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+}
