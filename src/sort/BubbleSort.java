ce8dd46e4a7e6d7cdcf57f5ac74550067ee46dd4
xiaojun.li <xiaojun.li@qlchat.com>
2022-04-13 22:45:23 +0800

    feat:冒泡排序

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,11 @@
+package sort;
+
+/**
+ * 冒泡排序
+ *
+ * @author kundy
+ * @date 2022/4/12 10:44 PM
+ */
+public class BubbleSort {
+
+}
--- src/sort/SelectSort.java
+++ src/sort/SelectSort.java
@@ -1,6 +1,8 @@
 package sort;
 
 /**
+ * 选择排序【每一轮，将最大/最小的元素放在已排序队列的末尾】
+ *
  * @author kundy
  * @date 2022/3/2 3:31 PM
  */
@@ -8,17 +10,37 @@ public class SelectSort {
 
     public static void main(String[] args) {
         Integer[] array = new Integer[]{5, 2, 6, 2, 7, 3, 9, 1};
+        print(sort(array));
+    }
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] > array[j]) {
-                    swap(array, i, j);
+    public static Integer[] sort(Integer[] array) {
+
+        int length = array.length;
+
+        // 为每一个元素找到它应该与哪一个元素交换位置
+        for (int i = 0; i < length; i++) {
+
+            // 假设当前元素就是最大值
+            int maxPos = i;
+
+            // 选出当前这一轮最大的元素下标
+            for (int j = i + 1; j < length; j++) {
+                if (array[j] > array[maxPos]) {
+                    maxPos = j;
                 }
             }
-        }
 
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            if (maxPos != i) {
+                swap(array, i, maxPos);
+            }
+
+        }
+        return array;
+    }
+
+    private static void print(Integer[] array) {
+        for (Integer integer : array) {
+            System.out.print(integer + " ");
         }
     }
 
