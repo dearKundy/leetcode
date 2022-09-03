cab68b04d3e699f70672bf2950fd007360a29454
xiaojun.li <xiaojun.li@qlchat.com>
2022-09-03 12:02:12 +0800

    feat:48. 旋转图像-翻转代替旋转

--- src/Rotate.java
+++ src/Rotate.java
@@ -6,6 +6,9 @@
  */
 public class Rotate {
 
+    /**
+     * 使用辅助数组
+     */
     public void rotate(int[][] matrix) {
 
         int row = matrix.length;
@@ -22,6 +25,35 @@ public class Rotate {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 matrix[i][j] = newMatrix[i][j];
+            }
+        }
+
+    }
+
+    /**
+     * 翻转代替旋转【水平翻转+主对角线翻转】
+     */
+    public void rotate2(int[][] matrix) {
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        // 水平翻转【因为是swap，所以对半】
+        for (int i = 0; i < row / 2; i++) {
+            for (int j = 0; j < col; j++) {
+                // swap
+                int tmp = matrix[row - i - 1][j];
+                matrix[row - i - 1][j] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+
+        // 主对角线翻转【swap，斜下方部分进行swap】
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
             }
         }
 
