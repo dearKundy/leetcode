0d52f3a4281834395363cbded095fe7a2e51adc8
xiaojun.li <xiaojun.li@qlchat.com>
2023-11-08 20:52:03 +0800

    feat：哈希-字母异位词分组

--- README.md
+++ README.md
@@ -54,7 +54,8 @@
 
 **LeetCode**
 
-- [1. 两数之和](src/TwoSum.java)
+- [1. 哈希-两数之和](src/TwoSum.java)
+- [49. 哈希-字母异位词分组](src/GroupAnagrams.java)
 - [2. 两数相加](src/AddTwoNumbers.java)
 - [9. 回文数](src/IsPalindrome.java)
 - [11. 盛最多水的容器](src/MaxArea.java)
---
+++ src/GroupAnagrams.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+/**
+ * 哈希-字母异位词分组
+ *
+ * @author kundy
+ * @date 2023/11/6 22:03
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> res = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            // 含有的字母全部相同的即为一个字母异位词，所以排序后拼接成一个字符串（规范化）
+            Arrays.sort(charArray);
+            String key = new String(charArray);
+
+            // 含有字母相同的放在同一个集合中
+            List<String> orDefault = res.getOrDefault(key, new ArrayList<>());
+            orDefault.add(str);
+
+            res.put(key, orDefault);
+
+        }
+
+        return new ArrayList<>(res.values());
+
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> lists = new GroupAnagrams().groupAnagrams(strings);
+
+        lists.forEach(System.out::println);
+    }
+
+
+}
