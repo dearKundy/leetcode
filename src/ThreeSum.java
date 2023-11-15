f08ff3a5d1a8129b3c0832493d07b565d17fc645
xiaojun.li <xiaojun.li@qlchat.com>
2023-11-15 15:02:20 +0800

    feat:三数之和

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -3,79 +3,85 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 15. 三数之和
+ * 15. 排序+双指针-三数之和
+ * <p>
+ * 先排序，必须要排序，才能使用双指针的方法，因为排序之后才能根据大小判断需要移动哪个指针，从而可以实现一次遍历找出所有的结果。
+ * 排序之后，去重也比较简单
+ *
+ * <p>
+ * 找匹配元素：
+ * 遍历每一个a，找b，c，使得a+b+c=0。b从a的下一个元素开始【左指针】，c从数组末尾开始【右指针】。
+ * 如果左右指针相加符合预期，加入结果集，左指针右移。
+ * 如果左右指针相加小于预期，左指针右移。
+ * 如果左右指针相加大于预期，右指针左移。
+ * <p>
+ * 结果集去重：
+ * a、b均要去重，核心思路就是从第二个元素开始，判断是否与前面一个元素相同，相同则跳过，因为元素相同，结果集肯定也相同。
  *
  * @author kundy
  * @date 2022/5/18 10:26 PM
  */
 public class ThreeSum {
 
-    public static void main(String[] args) {
-        List<List<Integer>> lists = wayOne(new int[]{-1, 0, 1, 2, -1, -4});
-        for (List<Integer> list : lists) {
-            for (Integer integer : list) {
-                System.out.print(integer + " ");
-            }
-            System.out.println("");
-        }
-    }
+    public List<List<Integer>> threeSum(int[] nums) {
 
-
-    public static List<List<Integer>> wayOne(int[] nums) {
-
-        if (nums == null || nums.length <= 0) {
+        if (nums == null || nums.length < 3) {
             return new ArrayList<>();
         }
 
+        // 从小到大排序
         Arrays.sort(nums);
 
         List<List<Integer>> res = new ArrayList<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
+        // 遍历a
+        for (int i = 0; i < nums.length; i++) {
 
-            // 重复跳过
+            // 因为是从小到大排序，如果当前元素大于0，则后面的元素肯定大于0，相加肯定不可能等于0，该元素后面的所有元素都不可能了，直接 break
+            if (nums[i] > 0) {
+                break;
+            }
+
+            // 如果当前元素的相反数比最大的两个元素相加还要大，那就无解了
+            if (-nums[i] > nums[nums.length - 1] + nums[nums.length - 2]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = -nums[i];
+
+            // a去重，第二位开始才需要去重
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            int leftPos = i + 1;
-            int rightPos = nums.length - 1;
+            // 遍历b
+            while (left < right) {
 
-            while (leftPos < rightPos) {
-
-                int curValue = nums[i];
-                int curLeft = nums[leftPos];
-                int curRight = nums[rightPos];
-                int curTotal = curValue + curLeft + curRight;
-
-                if (curTotal > 0) {
-                    rightPos--;
+                // b也要去重，从第二位开始，当前元素与上一个元素相同，则跳过
+                if (left > i + 1 && nums[left] == nums[left - 1]) {
+                    left++;
+                    continue;
                 }
 
-                if (curTotal < 0) {
-                    leftPos++;
+                int curValue = nums[left] + nums[right];
+                if (curValue == target) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
                 }
 
-                if (curTotal == 0) {
-                    res.add(Arrays.asList(curValue, curLeft, curRight));
-
-                    // 重复跳过
-                    while (leftPos < rightPos && nums[rightPos] == nums[rightPos - 1]) {
-                        rightPos--;
-                    }
-                    while (leftPos < rightPos && nums[leftPos] == nums[leftPos + 1]) {
-                        leftPos++;
-                    }
-
-                    // 继续找
-                    rightPos--;
-                    leftPos++;
+                // 比目标大，右指针左移
+                if (curValue > target) {
+                    right--;
+                } else {
+                    left++;
                 }
             }
 
         }
 
         return res;
+
     }
 
 }
