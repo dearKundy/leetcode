d2dee31e839d7c09fe8a01582798363b45fbd93f
xiaojun.li <xiaojun.li@qlchat.com>
2023-11-21 21:27:16 +0800

    feat:单调栈-接雨水

--- src/Trap.java
+++ src/Trap.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
- * 42. 接雨水【第二道纯靠自己 AC 的 difficult 题】
+ * 42. 单调栈-接雨水
  *
  * @author kundy
  * @date 2022/8/25 21:33
@@ -9,40 +11,39 @@ import java.util.Arrays;
 public class Trap {
 
     public static void main(String[] args) {
-        System.out.println(new Trap().trap(new int[]{0, 3, 0, 2, 1, 0, 1, 31, 2, 1, 2, 1}));
+        System.out.println(new Trap().trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
     }
 
     public int trap(int[] height) {
-        int maxHeight = Arrays.stream(height).max().getAsInt();
 
         int res = 0;
 
-        for (int i = 0; i < maxHeight; i++) {
-            int left = 0;
-            int right = height.length - 1;
+        Deque<Integer> stack = new LinkedList<>();
 
-            // 第N层的左墙
-            while (height[left] - i <= 0 && left < height.length - 1) {
-                left++;
-            }
+        for (int i = 0; i < height.length; i++) {
 
-            // 第N层的右墙
-            while (height[right] - i <= 0 && right > 0) {
-                right--;
-            }
+            // 不存在重复计算的问题，因为你会发现他进入一个区间后，都是在区间结束的时候才会进行计算一次。
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
 
-            // 统计每一层的容量
-            if (left < right) {
-                for (int j = left; j < right; j++) {
-                    if (height[j] - i <= 0) {
-                        res++;
-                    }
+                // 当前要求解的柱子
+                Integer pop = stack.pop();
+
+                if (stack.isEmpty()) {
+                    break;
                 }
+
+                // 有解，此时的栈顶元素就是左侧第一个比要求解的柱子高的元素
+                Integer left = stack.peek();
+                // height[i] 就是右侧第一个比要求解的柱子高的元素
+                int curHeight = Math.min(height[i], height[left]) - height[pop];
+                res += (i - left - 1) * curHeight;
             }
 
+            stack.push(i);
         }
 
         return res;
+
     }
 
 }
