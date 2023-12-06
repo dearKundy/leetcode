f15bf3bb421e546918cedccbf8b317706ade1781
xiaojun.li <xiaojun.li@qlchat.com>
2023-12-06 16:02:27 +0800

    feat:二叉搜索树中的搜索

---
+++ src/SearchBST.java
@@ -0,0 +1,45 @@
+/**
+ * 700. 二叉搜索树中的搜索
+ *
+ * @author kundy
+ * @date 2023/12/6 15:17
+ */
+public class SearchBST {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+        return searchBST(root.right, val);
+
+    }
+
+}
