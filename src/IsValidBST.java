e18218f92e8f32e84062cc5a96ba79a95e881dda
xiaojun.li <xiaojun.li@qlchat.com>
2023-12-12 14:56:01 +0800

    feat:验证二叉搜索树

--- src/IsValidBST.java
+++ src/IsValidBST.java
@@ -27,22 +27,28 @@ public class IsValidBST {
 
     private long pre = Long.MIN_VALUE;
 
+    /**
+     * 搜索树的特性就是左子树的值都小于根节点的值，右子树的值都大于根节点的值，所以这里我们在中序遍历的过程中，判断一下元素是不是单调递增即可
+     */
     public boolean isValidBST(TreeNode root) {
 
         if (root == null) {
             return true;
         }
 
-        boolean left = isValidBST(root.left);
+        if (!isValidBST(root.left)) {
+            return false;
+        }
 
         if (root.val <= pre) {
             return false;
         }
+
+        // 判断完之后，将pre值置为当前节点的值
         pre = root.val;
 
-        boolean right = isValidBST(root.right);
+        return isValidBST(root.right);
 
-        return left && right;
     }
 
 }
